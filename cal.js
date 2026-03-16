ae398b4cbfd594c4a64d8b64f80d87f68485b9fd
Nandan Mali <nandanmali5624@gmail.com>
2026-03-16 14:38:11 +0530

    latest

--- cal.js
+++ cal.js
@@ -2,26 +2,11 @@ let display=document.getElementById("display");
 c=0;
 function append(value){
     if (!isNaN(value)) {
-       if (c==1) {
-         display.value+=value;
-        op=value*(op/100);
-        c=2;
-        // display.value=op;
-       }
-       else{
         display.value+=value;
-        op=display.value;
-       }
+        op=display.value;      
     }
-    else{
-        if (value=='%') {
-            c=1;
+    else{        
             display.value=op+value;
-        }
-        else{
-            display.value=op+value;
-        }
-       
     }
 
 }
@@ -34,6 +19,9 @@ function removeLast(){
 let display = document.getElementById("display");
 display.value = display.value.slice(0,-1);
 op=display.value;
+}
+function percent(){
+    display.value=eval(display.value)/100;
 }
 
 function calculate(){
